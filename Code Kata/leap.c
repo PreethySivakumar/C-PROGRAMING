4191dee94809cd6d3375388e6535ea62e39ccd74
PreethySivakumar <38650903+PreethySivakumar@users.noreply.github.com>
2018-06-26 10:51:00 +0000

    Update leap.c

--- Code Kata/leap.c
+++ Code Kata/leap.c
@@ -1,16 +1,16 @@
 
-    #include<stdio.h>
+#include<stdio.h>
 int main()
 {
     int a;
     scanf("%d",&a);
     if((a%400==0) || a%4==0 && a%100!=0)
     {
-        printf("leap year");
+        printf("yes");
     }
     else
     {
-        printf("not");
+        printf("no");
     }
     return 0;
 }    
