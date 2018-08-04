1b505e1e3ab66994c28648202aeb61a5e5d23f3c
PreethySivakumar <38650903+PreethySivakumar@users.noreply.github.com>
2018-08-04 07:23:57 +0000

    Create primerange.c

---
+++ Code Kata/primerange.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,a,b,i,count;
+scanf("\n%d%d",&a,&b);
+for(i=a;i<b;i++)
+{
+count=0;
+for(n=2;n<i;n++)
+{
+if(i%n==0)
+count++;
+}
+if(count==0)
+printf("%d\n",i);
+}
+}
